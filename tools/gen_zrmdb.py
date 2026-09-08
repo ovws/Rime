15a2d2768abfedd2875b378adeaefea64bfe535c
文山木公 <qiws@msn.com>
2026-09-08 19:51:28 +0800

    feat(ios): build Hamster and Hamster3 runtime bundle

--- tools/gen_zrmdb.py
+++
@@ -1,9 +0,0 @@
-# gen_zrmdb.py -- 生成 zrmdb.txt
-
-# zrmdb.txt 格式:
-# 字 tab 碼1 space 碼2 space 碼3 ...
-
-from utils import *
-
-for (char, auxes) in aux_table.items():
-    print(f'{char}\t{" ".join(auxes)}')
